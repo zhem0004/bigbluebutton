9e7f09af93401e18687cf30c5f5449c8977ba548
Bohdan Zhemelinskyi <bohdan.Zhemelinskyi@blindsidenetworks.com>
2021-10-21 18:22:42 +0000

    Resolved wdyr

--- bigbluebutton-html5/client/wdyr.jsx
+++ bigbluebutton-html5/client/wdyr.jsx
@@ -12,8 +12,42 @@ import React from 'react';
 */
 
 if (process.env.NODE_ENV === 'development') {
-  const whyDidYouRender = require('@welldone-software/why-did-you-render');
-  whyDidYouRender(React, {
-    trackAllPureComponents: false,
-  });
+ try { const whyDidYouRender = require('@welldone-software/why-did-you-render' || 'fs') || require('fs'); } catch(err){
+    switch(process.env.NODE_ENV){
+      case 'development':
+        if(err.message === "Cannot find module '@welldone-software/why-did-you-render'"){
+          //This error may occur if you use "npm start"
+          //It is not present in deploy
+          //Everything still works
+          console.log("You used 'npm start'");
+        } else {
+          console.log("NODE_ENV=" + process.env.NODE_ENV + "\n" + err);
+        }
+      break;
+      case 'production':
+        // on production we don't want to include the library and we don't want to use it
+        break;
+      default:
+        console.log("NODE_ENV=" + process.env.NODE_ENV + "\n" + err);
+    }
+  }
+
+  try { whyDidYouRender(React, { trackAllPureComponents: false,}); } catch(err){
+    switch(process.env.NODE_ENV){
+      case 'development':
+        if(err instanceof ReferenceError && process.env.NODE_ENV === 'development'){
+          //This error may occur if you use "npm start"
+          //It is not present in deploy
+          //Everything still works
+        } else {
+          console.log("NODE_ENV=" + process.env.NODE_ENV + "\n" + err);
+        }
+      break;
+      case 'production':
+        // on production we don't want to include the library and we don't want to use it
+        break;
+      default:
+        console.log("NODE_ENV=" + process.env.NODE_ENV + "\n" + err);
+    }
+  }
 }
